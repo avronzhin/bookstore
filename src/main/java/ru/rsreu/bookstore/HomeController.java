15b355a487df5567728826662d42822e4f78464e
AleksejRonzhin <aleksej.ronzhin.rsreu@gmail.com>
2022-09-07 01:55:41 +0300

    Added home page

---
+++ src/main/java/ru/rsreu/bookstore/HomeController.java
@@ -0,0 +1,12 @@
+package ru.rsreu.bookstore;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+}
