adddbdf281950110f064d8b1b4068624887ec273
Yuri <uendeligz@gmail.com>
2023-11-25 18:30:14 +0300

    Add navigation collection to related books

--- src/main/java/ru/rsreu/bookstore/books/models/Genre.java
+++ src/main/java/ru/rsreu/bookstore/books/models/Genre.java
@@ -5,14 +5,29 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @Entity
 @NoArgsConstructor
-@AllArgsConstructor
 public class Genre {
+
     @Id
     private String id;
+
+    @NotNull
     private String title;
+
+    @ManyToMany
+    private List<Book> booksOfThisGenre = new ArrayList<>();
+
+    public Genre(String id, String title) {
+        this.id = id;
+        this.title = title;
+    }
 }
