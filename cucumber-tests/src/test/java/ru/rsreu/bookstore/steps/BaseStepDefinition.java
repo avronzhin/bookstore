58b41e16ad93fca8cd9d5a46f5932a7e6c6f0d93
Yuri <uendeligz@gmail.com>
2023-11-25 18:02:17 +0300

    Rename step methods
    According to test case naming convention

--- cucumber-tests/src/test/java/ru/rsreu/bookstore/steps/BaseStepDefinition.java
+++ cucumber-tests/src/test/java/ru/rsreu/bookstore/steps/BaseStepDefinition.java
@@ -11,41 +11,41 @@ public class BaseStepDefinition {
     private final static String host = "http://localhost:8080";
 
     @Given("пользователь открыл страницу {string}")
-    public void openPage(String pageUrl) {
+    public void user_opened_page(String pageUrl) {
         Selenide.open(getAbsoluteUrl(pageUrl));
         Selenide.sleep(5000);
     }
 
     @When("пользователь нажимает кнопку {string}")
-    public void pressButton(String buttonValue) {
+    public void user_pressed_button(String buttonValue) {
         var xPath = "//button[text()='" + buttonValue + "']";
         var button = Selenide.$x(xPath);
         button.click();
     }
 
     @Then("пользователь нажимает на ссылку {string}")
-    public void pressLink(String linkValue){
+    public void user_pressed_link(String linkValue){
         var xPath = "//a[text()='" + linkValue + "']";
         var link = Selenide.$x(xPath);
         link.click();
     }
 
     @Then("открыта страница {string}")
-    public void pageIsOpen(String pageUrl) {
+    public void page_is_open(String pageUrl) {
         var currentUrl = Selenide.webdriver().driver().url();
         var expectedUrl = getAbsoluteUrl(pageUrl);
         Assert.assertEquals(expectedUrl, currentUrl);
     }
 
     @And("пользователь авторизован под логином {string}")
-    public void userAuthorized(String login) {
+    public void user_authorized_under_login(String login) {
         var xPath = "//span[text()='" + login + "']";
         var span = Selenide.$x(xPath);
         Assert.assertTrue(span.isDisplayed());
     }
 
     @And("пользователь не авторизован")
-    public void userNotAuthorized() {
+    public void user_not_authorized() {
         var xPath = "//span";
         var span = Selenide.$x(xPath);
         Assert.assertFalse(span.exists());
@@ -56,21 +56,21 @@ public class BaseStepDefinition {
     }
 
     @When("пользователь вводит в поле ввода {string} значение {string}")
-    public void addValueInInput(String inputId, String value) {
+    public void user_enters_a_value_in_input_field(String inputId, String value) {
         var xPath = "//input[@id='" + inputId + "']";
         var input = Selenide.$x(xPath);
         input.setValue(value);
     }
 
     @And("выведено сообщение {string}")
-    public void messageIsDisplayed(String message) {
+    public void message_is_displayed(String message) {
         var xPath = "//*[contains(text(), '" + message + "')]";
         var messageElement = Selenide.$x(xPath);
         Assert.assertTrue(messageElement.isDisplayed());
     }
 
     @And("пользователь нажимает кнопку формы {string}")
-    public void pressFormButton(String buttonValue) {
+    public void user_press_form_button(String buttonValue) {
         var xPath = "//input[@value='" + buttonValue + "']";
         var button = Selenide.$x(xPath);
         button.click();
